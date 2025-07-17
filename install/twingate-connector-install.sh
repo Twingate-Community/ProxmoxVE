aca74bb059226b83f85d61d74d42c9417d1473b5
Andrew Baumbach <andrewb@twingate.com>
2025-07-17 11:56:22 -0400

    chore: connector doesn't seem to be starting up. Reworking to flush out errors

--- install/twingate-connector-install.sh
+++ install/twingate-connector-install.sh
@@ -42,12 +42,19 @@ while true; do
 done
 
 msg_info "Setting up Twingate Connector"
-$STD curl "https://binaries.twingate.com/connector/setup.sh" | sudo TWINGATE_ACCESS_TOKEN="${access_token}" TWINGATE_REFRESH_TOKEN="${refresh_token}" TWINGATE_NETWORK="${network}" TWINGATE_LABEL_DEPLOYED_BY="linux" bash
 
+export TWINGATE_ACCESS_TOKEN="${access_token}"
+export TWINGATE_REFRESH_TOKEN="${refresh_token}"
+export TWINGATE_NETWORK="${network}"
+export TWINGATE_LABEL_DEPLOYED_BY="linux"
+
+bash -c $(curl "https://binaries.twingate.com/connector/setup.sh")
 if [[ $? -ne 0 ]]; then
     msg_error "Failed to set up Twingate Connector. Please check your tokens and network name."
     exit 1
 fi
+
+msg_info "Twingate Connector status: $(systemctl status twingate-connector)"
 
 apt-get -y autoremove
 apt-get -y autoclean
