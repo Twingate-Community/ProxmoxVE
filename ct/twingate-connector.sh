e8d7043a92119ae9dd030ecf0b65def7154aa1cf
Andrew Baumbach <abaumbach@Andrews-MBP.localdomain>
2025-07-17 10:29:54 -0400

    feat: first pass at main app script

---
+++ ct/twingate-connector.sh
@@ -0,0 +1,47 @@
+#!/usr/bin/env bash
+source <(curl -fsSL https://raw.githubusercontent.com/twingate-andrewb/ProxmoxVE/refs/heads/feat/twingate-connector/misc/build.func)
+# Copyright (c) 2021-2025 community-scripts ORG
+# Author: twingate-andrewb
+# License: MIT | https://github.com/community-scripts/ProxmoxVE/raw/main/LICENSE
+# Source: https://www.twingate.com/docs/
+
+APP="twingate-connector"
+var_tags="${var_tags:-network;connector;twingate}"
+var_cpu="${var_cpu:-1}"
+var_ram="${var_ram:-512}"
+var_disk="${var_disk:-2}"
+var_os="${var_os:-ubuntu}"
+var_version="${var_version:-22.04}"
+var_unprivileged="${var_unprivileged:-1}"
+
+header_info "$APP"
+variables
+color
+catch_errors
+
+function update_script() {
+  header_info
+  check_container_storage
+  check_container_resources
+
+  if [[ ! -d /lib/systemd/system/twingate-connector.service ]]; then
+      msg_error "No ${APP} Installation Found!"
+      exit
+  fi
+
+  msg_info "Updating ${APP}"
+
+  apt update
+  apt install -yq twingate-connector
+  systemctl restart twingate-connector
+
+  msg_ok "Updated Successfully"
+  exit
+}
+
+start
+build_container
+description
+
+msg_ok "Completed Successfully!\n"
+echo -e "${CREATING}${GN}${APP} setup has been successfully initialized!${CL}"
